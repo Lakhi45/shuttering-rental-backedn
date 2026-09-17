a56eb6a6b66a6a6998b3804b1e929428595d3afe
Lakhi45 <141494693+Lakhi45@users.noreply.github.com>
2026-09-17 21:24:42 +0530

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,24 +1,3 @@
 const app = require('../src/app');
-const connectDB = require('../src/config/db');
 
-let isConnected = false;
-
-module.exports = async (req, res) => {
-  try {
-    // Connect to MongoDB only when needed
-    if (!isConnected) {
-      await connectDB();
-      isConnected = true;
-    }
-
-    // Pass request to Express
-    return app(req, res);
-  } catch (error) {
-    console.error('Server error:', error);
-
-    return res.status(500).json({
-      success: false,
-      message: 'Internal server error',
-    });
-  }
-};
+module.exports = app;
